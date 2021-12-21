7d0bb5e3affbae6648404cb16f9631df5c292815
Alisa Proshchenko <alisa.proshchenko@valtech.com>
2021-12-21 17:17:31 +0200

    added user class and identity context

---
+++ IdentityService/Contexts/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using IdentityService.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityService.Contexts
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext() : base("IdentityDB")
+        {
+            
+        }
+    }
+}
---
+++ IdentityService/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityService.Models
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool Banned { get; set; }
+    }
+}
