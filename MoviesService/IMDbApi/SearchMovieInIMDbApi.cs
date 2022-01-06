27840c0de22d2c6fdaa64a015459908bbb88786b
Mykhailo Chudik <mykhailo.chudik@valtech.com>
2022-01-06 02:33:19 +0200

    AddAMDbSearch

---
+++ MoviesService/IMDbApi/SearchMovieInIMDbApi.cs
@@ -0,0 +1,54 @@
+﻿using IMDbApiLib;
+using MoviesService.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IMDbApiLib.Models;
+using MoviesService.Models;
+
+namespace MoviesService.IMDbApi
+{
+    public class SearchMovieInIMDbApi
+    {
+        private static ApiLib _apiLib;
+        private  ConvertorApiData _convertor = new ConvertorApiData();
+
+        public SearchMovieInIMDbApi()
+        {
+            _apiLib = new ApiLib("k_d99sty8t");
+        }
+
+        public SearchMovieInIMDbApi(string key)
+        {
+            _apiLib = new ApiLib(key);
+        }
+
+        public async Task<MediaDto> SearchAsynTask(string name)
+        {
+            var dataApi = await _apiLib.SearchAsync(name);
+
+            var searchResults = dataApi.Results;
+
+            var movieData = await _apiLib.TitleAsync(searchResults[0].Id);
+
+            var model = new MediaDto
+            {
+                IMDbMovieId = movieData.Id,
+                Name = movieData.Title,
+                Poster = movieData.Image,
+                Year = Convert.ToInt32(movieData.Year),
+                Cast = _convertor.Actors(movieData.ActorList),
+                Plot = movieData.Plot,
+                BudgetAndBoxOffice = _convertor.Budget(movieData.BoxOffice.Budget),
+                Types = new Types { Name = movieData.Type },
+                RatingIMDb = Convert.ToDouble(movieData.IMDbRating),
+                CountryCollection = _convertor.Countries(movieData.Countries),
+                GenresCollection = _convertor.Genres(movieData.GenreList)
+            };
+
+            return model;
+        }
+    }
+}
