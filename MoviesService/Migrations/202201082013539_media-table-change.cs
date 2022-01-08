359f2fbf6cb2ef4a7cade36cb6e41a7659c38b89
Vladyslav Levchenko <vladyslav.levchenko@valtech.com>
2022-01-08 23:09:13 +0200

    Levchenko V.O. like-watched-start-working

---
+++ MoviesService/Migrations/202201082013539_media-table-change.cs
@@ -0,0 +1,18 @@
+﻿namespace MoviesService.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mediatablechange : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Media", "SiteUsersRatings", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Media", "SiteUsersRatings", c => c.Int(nullable: false));
+        }
+    }
+}
