a68c39a73e53d1a9fd7643fc7db29b28724e2c21
Vladyslav Levchenko <vladyslav.levchenko@valtech.com>
2022-01-11 13:02:43 +0200

    Levchenko V.O style cover 95%

--- MoviesService/Search/SearchInDataBase.cs
+++ MoviesService/Search/SearchInDataBase.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Collections.Generic;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MoviesService.Context;
-using MoviesService.Dto;
 using MoviesService.IMDbApi;
 using MoviesService.Models;
 
