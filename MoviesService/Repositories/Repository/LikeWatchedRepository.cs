43b25b1a06cdc7bc3d71d92af41b0a38271efdd1
Vladyslav Levchenko <vladyslav.levchenko@valtech.com>
2022-01-24 12:41:34 +0200

    One more time fix

--- MoviesService/Repositories/Repository/LikeWatchedRepository.cs
+++ MoviesService/Repositories/Repository/LikeWatchedRepository.cs
@@ -34,7 +34,7 @@ namespace MoviesService.Repositories.Repository
 
             if (media.AmountOfLikes != 0)
             {
-                media.SiteUsersRatings = 10.0 - (Convert.ToDouble(usersAmount) / Convert.ToDouble(media.AmountOfLikes)) < 0 ? 0 : Math.Round(10.0 - (Convert.ToDouble(usersAmount) / Convert.ToDouble(media.AmountOfLikes)),1);
+                media.SiteUsersRatings = 10.0 - (Convert.ToDouble(usersAmount) / Convert.ToDouble(media.AmountOfLikes)) < 0 ? 0 : Math.Round(10.0 - ((Convert.ToDouble(usersAmount) / Convert.ToDouble(media.AmountOfLikes) == 1 ? 0 : Convert.ToDouble(usersAmount) / Convert.ToDouble(media.AmountOfLikes))),1);
             }
             else
             {
